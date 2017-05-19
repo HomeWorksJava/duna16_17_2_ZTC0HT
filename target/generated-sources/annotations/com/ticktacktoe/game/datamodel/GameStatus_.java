ac647dc4b024d68eae45a2844de99fef25ce47cd
bartnorb <norbi.bartalos@gmail.com>
2017-05-19 18:24:01 +0200

    Add files via upload

---
+++ target/generated-sources/annotations/com/ticktacktoe/game/datamodel/GameStatus_.java
@@ -0,0 +1,24 @@
+package com.ticktacktoe.game.datamodel;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(GameStatus.class)
+public abstract class GameStatus_ {
+
+	public static volatile SingularAttribute<GameStatus, String> row1_col0;
+	public static volatile SingularAttribute<GameStatus, String> row1_col1;
+	public static volatile SingularAttribute<GameStatus, String> row1_col2;
+	public static volatile SingularAttribute<GameStatus, String> row2_col2;
+	public static volatile SingularAttribute<GameStatus, String> row2_col1;
+	public static volatile SingularAttribute<GameStatus, String> row0_col2;
+	public static volatile SingularAttribute<GameStatus, String> row0_col1;
+	public static volatile SingularAttribute<GameStatus, String> activePlayer;
+	public static volatile SingularAttribute<GameStatus, String> row0_col0;
+	public static volatile SingularAttribute<GameStatus, String> row2_col0;
+	public static volatile SingularAttribute<GameStatus, Integer> id;
+
+}
+
